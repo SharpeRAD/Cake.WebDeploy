e6fb7156f66bdec579657ff74f4825644c5f8fc9
Phillip Sharpe <code@sharperad.com>
2015-09-21 20:51:31 +0100

    Install WebDeploy during AppVeyor build

---
+++ install.ps1
@@ -0,0 +1,14 @@
+#Download and install Chocolatey
+iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
+
+## Install WebPI
+cInst webpicommandline -h
+
+#Install WebSeploy
+$webPiProducts = @('webdeploy') 
+WebPICMD /Install /Products:"$($webPiProducts -join ',')" /AcceptEULA
+
+#Start Services
+Start-Service W3SVC
+Start-Service WMSVC
+Start-Service msdepsvc
